034d176820093f9afd6fad4ee2688376d1164c23
Maria Reedstrom <maria.reedstrom@gmail.com>
2022-07-18 18:11:53 +0200

    commit tree c1bf07401bf73e48ce76a3e84fabfdafd241d863

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,7 +2,21 @@ class StudentsController < ApplicationController
 
   def index
     students = Student.all
+
+    if params[:first_name]
+      students = students.where(first_name: params[:first_name])
+    end
+
+    if params[:last_name]
+      students = students.where(last_name: params[:last_name])
+    end
+
     render json: students
+  end
+
+  def show
+    student = Student.find_by(id: params[:id])
+    render json: student
   end
 
 end
